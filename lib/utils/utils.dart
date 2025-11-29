5352491e5d5917d022a6c02d9f56fcdf44ed8c17
daoporo102 <truongquangdao102@gmail.com>
2025-11-29 22:27:50 +0700

    feat: add pick image function in Utils

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/foundation.dart';
+import 'package:image_picker/image_picker.dart';
 
 void avoidPrint(String s) {
   if (kDebugMode) {
     print(s);
   }
+}
+
+Future<Uint8List?> pickImage(ImageSource source) async {
+  // Your image picking logic here
+  final ImagePicker imagePicker = ImagePicker();
+  final XFile? file = await imagePicker.pickImage(source:  source);
+  if (file==null) {
+    avoidPrint("No image selected");
+    return null;
+  }
+  return await file.readAsBytes();
 }
