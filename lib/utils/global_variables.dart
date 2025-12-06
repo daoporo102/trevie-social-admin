5c19eea0ce9a8b678fca4ba0145ba7e6d70dac13
daoporo102 <truongquangdao102@gmail.com>
2025-12-06 21:51:58 +0700

    add emailRegex to global variables

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:social_media_admin/utils/colors.dart';
 
+final emailRegex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+
 Widget customCircularProgressIndicator() {
   return Center(
     child: CircularProgressIndicator(
