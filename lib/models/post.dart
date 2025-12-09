18545c22c2060eb1dcc2c862f6f36a4fe6b1327a
daoporo102 <truongquangdao102@gmail.com>
2025-12-09 15:32:51 +0700

    add role property to Post model

--- lib/models/post.dart
+++ lib/models/post.dart
@@ -20,6 +20,7 @@ class Post {
   final String? originalPostText;
   final String? originalDisplayName;
   final String? originalProfImage;
+  final String role;
 
   const Post({
     required this.postId,
@@ -39,6 +40,7 @@ class Post {
     this.originalDisplayName,
     this.originalProfImage,
     required this.likesCount,
+    this.role = 'user',
   });
 
   Map<String, dynamic> toJson() => {
@@ -61,6 +63,7 @@ class Post {
     "originalDisplayName": originalDisplayName,
     "originalProfImage": originalProfImage,
     "likesCount": likes.length,
+    "role": role,
   };
 
   static Post fromSnap(DocumentSnapshot snapshot) {
@@ -107,6 +110,7 @@ class Post {
       originalDisplayName: snapshotData['originalDisplayName'],
       originalProfImage: snapshotData['originalProfImage'],
       likesCount: snapshotData['likesCount'] ?? snapshotData['likes']?.length ?? 0,
+      role: snapshotData['role'] ?? 'user',
     );
   }
 }
