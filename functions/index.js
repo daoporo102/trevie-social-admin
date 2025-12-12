5dadaedb4851cf69f23e29202c8703f099e104f6
daoporo102 <truongquangdao102@gmail.com>
2025-12-12 16:13:54 +0700

    feat(moderate-posts-text): add checkPostUpdate function to check Post Update with AI

--- functions/index.js
+++ functions/index.js
@@ -10,6 +10,7 @@ const {setGlobalOptions} = require("firebase-functions/v2");
 const admin = require("firebase-admin");
 const {onDocumentCreated} = require("firebase-functions/v2/firestore");
 const axios = require("axios");
+const {onDocumentUpdated} = require("firebase-functions/v2/firestore");
 
 // AI Server URL
 const AI_SERVER_URL = "https://peripherally-subovoid-doug.ngrok-free.dev/predict";
@@ -342,6 +343,104 @@ exports.checkPostText = onDocumentCreated("posts/{postId}", async (event) => {
 
     // On error, Auto-Approve the post to avoid pending content for users
     await snapshot.ref.update({
+      status: "active",
+      moderatedBy: "system_failover",
+      aiReason: "Dich vụ AI lỗi -> tự động duyệt",
+      moderatedAt: admin.firestore.Timestamp.now(),
+    });
+    console.log(` Đã Auto-Approve bài viết do lỗi.`);
+  }
+});
+
+/**
+ * 8. Check Post Update (when user updates post text)
+ */
+exports.checkPostUpdate = onDocumentUpdated("posts/{postId}", async (event) => {
+  // Get data before and after the update
+  const beforeDoc = event.data.before;
+  const afterDoc = event.data.after;
+  const postId = event.params.postId;
+
+  // get data (json contains content)
+  const beforeData = beforeDoc.data();
+  const afterData = afterDoc.data();
+
+  // Get new text
+  const newText = afterData.postText || "";
+  const oldText = beforeData.postText || "";
+
+  // If text hasn't changed, exit
+  if (newText === oldText) {
+    console.log(`Bài ${postId} cập nhật nhưng không thay đổi nội dung văn bản. Bỏ qua kiểm tra AI.`);
+    return;
+  }
+
+  // If text changed, proceed to check with AI
+  console.log(`[UPDATE] Bài ${postId} đã thay đổi nội dung văn bản. Gửi lại tới AI để kiểm tra...`);
+
+  if (!newText) {
+    return afterDoc.ref.update({status: "active"});
+  }
+
+  // start time
+  const startTime = Date.now();
+  console.log(`[START] Bắt đầu gửi bài ${postId} tới AI...`);
+
+  try {
+    console.log(`Đang gửi bài ${postId} tới AI Server...`);
+    // call AI server with timeout of 10 seconds
+    const response = await axios.post(AI_SERVER_URL, {
+      text: newText,
+    }, {timeout: 10000});
+
+    // end time
+    const endTime = Date.now();
+    // Calculate execution time
+    const executionTime = endTime - startTime;
+
+    // Print the log (You will see this in the Console). measured in milliseconds
+    console.log(`[PERFORMANCE] AI phản hồi trong: ${executionTime}ms`);
+
+    // get AI result
+    const aiResult = response.data;
+    // process AI result
+    if (aiResult.is_toxic === true) {
+      // if toxic,
+      await afterDoc.ref.update({
+        postText: oldText,
+        status: "active",
+        // update failed
+        updateStatus: "failed",
+        updateError: `Cập nhật đã bị AI từ chối vì: ${aiResult.reason}`,
+        // get old time
+        lastDateModified: beforeData.lastDateModified,
+        dateUpdated: beforeData.dateUpdated,
+        // Update time check
+        moderatedAt: admin.firestore.Timestamp.now(),
+        moderatedBy: "AI_Rollback",
+      });
+      console.log(`AI chặn bài ${postId} vì: ${aiResult.reason} (mất ${executionTime}ms)`);
+      console.log(`Đã khôi phục bài viết về trạng thái cũ an toàn.`);
+    } else {
+      // if not toxic, set status to active
+      await afterDoc.ref.update({
+        status: "active",
+        aiReason: null,
+        updateStatus: "success",
+        updateError: null,
+        moderatedBy: "AI_Update",
+        moderatedAt: admin.firestore.Timestamp.now(),
+      });
+      console.log(`AI duyệt sạch (mất ${executionTime}ms)`);
+    }
+  } catch (error) {
+    // Measure the time even if there is an error (to know how long it takes to die)
+    // for example, if it takes exactly 10000ms, it's due to a timeout).
+    const errorTime = Date.now() - startTime;
+    console.error(`[ERROR] Lỗi gọi AI Server sau ${errorTime}ms:`, error.message);
+
+    // On error, Auto-Approve the post to avoid pending content for users
+    await afterDoc.ref.update({
       status: "active",
       moderatedBy: "system_failover",
       aiReason: "Dich vụ AI lỗi -> tự động duyệt",
